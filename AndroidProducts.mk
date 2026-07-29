d6afa01a2f688f5dc6fb15059a2f496620238a15
abobasik12334 <topkirill789@gmail.com>
2026-07-29 18:09:01 +0400

    Add COMMON_LUNCH_CHOICES to AndroidProducts.mk and enforce target selection

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,16 @@
+#
+# Copyright (C) 2026 The OrangeFox / TWRP Recovery Project
+#
+
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_jason.mk \
+    $(LOCAL_DIR)/twrp_jason.mk \
     $(LOCAL_DIR)/fox_jason.mk \
-    $(LOCAL_DIR)/twrp_jason.mk
+    $(LOCAL_DIR)/omni_jason.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_jason-eng \
+    twrp_jason-userdebug \
+    fox_jason-eng \
+    fox_jason-userdebug \
+    omni_jason-eng \
+    omni_jason-userdebug
