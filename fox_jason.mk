d66c738f0430feccba042a63f9e1502974dd82d4
abobasik12334 <topkirill789@gmail.com>
2026-07-29 18:17:05 +0400

    Use inherit-product-if-exists for embedded.mk across all product files

--- fox_jason.mk
+++ fox_jason.mk
@@ -4,7 +4,8 @@
 
 PRODUCT_RELEASE_NAME := jason
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/base_system.mk)
 
 $(call inherit-product-if-exists, vendor/fox/config/common.mk)
 $(call inherit-product-if-exists, vendor/twrp/config/common.mk)
